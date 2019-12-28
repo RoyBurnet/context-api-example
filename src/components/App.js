219cb617cbf69ae094b6838b94c0f49af9c34d76
Randy Burnet <rrw.burnet@gmail.com>
2019-12-28 18:06:06 +0100

    create layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import UserCreate from '../components/UserCreate'
 
-export default class App extends Component {
+class App extends Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language: language });
+  };
+
   render() {
     return (
       <div className="ui container">
-        App
+        Select a language:
+        <i
+          className="flag us"
+          onClick={() => this.onLanguageChange('english')}
+        />
+        <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
+        <UserCreate/>
       </div>
-    )
+    );
   }
 }
+
+export default App;
